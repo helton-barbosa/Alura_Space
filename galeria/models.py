0049ad3322e8361469a3ff3d54b0426600577619
Helton Barbosa <heltonbs@gmail.com>
2024-05-30 15:13:55 -0300

    Mostrar a data do cadastro

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime 
 
 
 class Fotografia(models.Model):
@@ -8,31 +9,42 @@ class Fotografia(models.Model):
         ("GALÁXIA", "Galáxia"),
         ("PLANETA", "Planeta")
     ]
+
     nome = models.CharField(
         max_length=100,
         null=False,
         blank=False
     )
+
     legenda = models.CharField(
         max_length=150,
         null=False,
         blank=False
     )
+
     categoria = models.CharField(
         max_length=100,
         choices=OPCOES_CATEGORIA,
         default=""
     )
+
     descricao = models.TextField(
         null=False,
         blank=False
     )
+
     foto = models.CharField(
         max_length=100,
         null=False,
         blank=False
     )
+
     publicada = models.BooleanField(default=False)
+
+    data_fotografia = models.DateTimeField(
+        default=datetime.now,
+        blank=False
+    )
 
     def __str__(self) -> str:
         return f'Fotografia [nome={self.nome}]'
