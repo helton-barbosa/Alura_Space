2cac6fd0af18fb80f23c41d0dad6ca84a992461a
Helton Barbosa <heltonbs@gmail.com>
2024-05-31 09:25:39 -0300

    Estilizando o formulário de Cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -25,3 +25,53 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome completo",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: José da Silva"
+            }
+        ),
+    )
+
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: jose.silva@example.com"
+            }
+        )
+    )
+
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from usuarios.forms import LoginForms
+from usuarios.forms import LoginForms, CadastroForms
 
 
 def login(request):
@@ -8,4 +8,5 @@ def login(request):
 
 
 def cadastro(request):
-    return render(request, "usuarios/cadastro.html")
+    form = CadastroForms()
+    return render(request, "usuarios/cadastro.html", {"form": form})
